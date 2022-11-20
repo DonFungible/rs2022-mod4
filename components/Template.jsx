94e1ec8b448e127e09f3643e07e262da3ddd605c
Don <100505855+DonFungible@users.noreply.github.com>
2022-11-19 21:27:52 -0800

    Initial commit

---
+++ components/Template.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Template() {
+    return (
+        <div className="border-red-500 border-4 w-2/3 mx-auto pt-24">
+            Template
+        </div>
+    );
+}
